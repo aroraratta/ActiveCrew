df4a6ce9c176997823f7411647b06abcfdb34163
EC2 Default User <ec2-user@ip-172-31-34-246.ap-northeast-1.compute.internal>
2024-12-04 15:14:58 +0000

    [Update]画像プレビューjquery修正

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,3 +1,5 @@
+// turboによってページ遷移時にjavascriptがうまく動作しない可能性がある。だからturbolinks:loadの記述は消さないように。
+
 // ハンバーガーバー用jquery
 document.addEventListener('turbolinks:load', function() {
   $('.menu-trigger').off('click').on('click', function(event) {
@@ -8,15 +10,17 @@ document.addEventListener('turbolinks:load', function() {
 });
 
 // ファイルプレビュー用jquery
-$(document).ready(function () {
-  $('#image_input').on('change', function (e) {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (event) {
-        $('#img_prev').attr('src', event.target.result);
-      };
-      reader.readAsDataURL(file);
-    }
+document.addEventListener('turbolinks:load', function() {
+  $(document).ready(function () {
+    $('#image_input').on('change', function (e) {
+      const file = e.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = function (event) {
+          $('#img_prev').attr('src', event.target.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    });
   });
 });
