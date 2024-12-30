27dcfad03c94a5cf8c67db4fe8e3741a8aa5425c
EC2 Default User <ec2-user@ip-172-31-34-246.ap-northeast-1.compute.internal>
2024-12-30 08:47:41 +0000

    [Add]検索機能で活動場所で検索できる機能実装

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,23 +5,37 @@ class SearchesController < ApplicationController
 
     # 管理者であればすべて検索可能、ユーザーであればis_activeのユーザーのみ検索可能
     if admin_signed_in?
-      if @range == "user"
+      case @range
+      when "user"
         @users = User.looks(params[:search], params[:word]).order(created_at: :desc)
-      elsif @range == "post"
+      when "post"
         @posts = Post.looks(params[:search], params[:word]).order(created_at: :desc)
-      elsif @range == "circle"
-        @circles = Circle.looks(params[:search], params[:word]).order(created_at: :desc)
+      when "circle"
+        @circles = Circle.looks_with_location(
+          params[:search],
+          params[:word],
+          params[:prefecture_id],
+          params[:city_id]
+        ).order(created_at: :desc)
       else
         flash[:alert] = "不正な検索範囲が指定されました。"
         redirect_to top_path
       end
     else
-      if @range == "user"
-        @users = User.looks(params[:search], params[:word]).where(is_active: true).order(created_at: :desc)
-      elsif @range == "post"
-        @posts = Post.looks(params[:search], params[:word]).joins(:user).where(users: { is_active: true }).order(created_at: :desc)
-      elsif @range == "circle"
-        @circles = Circle.looks(params[:search], params[:word]).order(created_at: :desc)
+      case @range
+      when "user"
+        @users = User.looks(params[:search], params[:word])
+                      .where(is_active: true).order(created_at: :desc)
+      when "post"
+        @posts = Post.looks(params[:search], params[:word])
+                      .joins(:user).where(users: { is_active: true }).order(created_at: :desc)
+      when "circle"
+        @circles = Circle.looks_with_location(
+          params[:search],
+          params[:word],
+          params[:prefecture_id],
+          params[:city_id]
+        ).order(created_at: :desc)
       else
         flash[:alert] = "不正な検索範囲が指定されました。"
         redirect_to top_path
